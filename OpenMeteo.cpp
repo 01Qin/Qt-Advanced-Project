65f55ad7c0ecc0736f22e1dbaf899da203a15f42
01Qin <sisukasqin@hotmail.com>
2026-04-14 03:02:34 +0300

    Add fetchHistoryData function

--- OpenMeteo.cpp
+++ OpenMeteo.cpp
@@ -13,6 +13,7 @@ OpenMeteo::OpenMeteo(EnvironmentModel *environment, QObject *parent)
 
 void OpenMeteo::start(){
     fetchData();
+    fetchHistoryData();
     m_Timer.start();
 
 }
@@ -24,8 +25,7 @@ void OpenMeteo::stop(){
 
 void OpenMeteo::fetchData(){
     QUrl url("https://api.open-meteo.com/v1/forecast"
-                "?latitude=52.52"
-                "&longitude=13.41"
+                "?latitude=52.52&longitude=13.41"
                 "&current=temperature_2m,relative_humidity_2m"
 
              );
@@ -48,6 +48,48 @@ void OpenMeteo::fetchData(){
         m_environment->setTemperature(temperature);
         m_environment->setHumidity(humidity);
         m_environment->setSource("Open-Meteo");
+
+        reply->deleteLater();
+
+    });
+}
+
+void OpenMeteo::fetchHistoryData(){
+    QUrl url("https://api.open-meteo.com/v1/forecast"
+             "?latitude=52.52&longitude=13.41"
+             "&past_days=10"
+             "&daily=temperature_2m_max,temperature_2m_min"
+             "&relative_humidity_2m_mean"
+             "&timezone=auto"
+
+             );
+
+    QNetworkRequest request(url);
+    auto reply = m_network.get(request);
+
+    connect(reply, &QNetworkReply::finished, this, [this,reply] {
+        if (reply->error() != QNetworkReply::NoError){
+            reply->deleteLater();
+            return;
+        }
+
+        const QJsonDocument doc = QJsonDocument::fromJson(reply->readAll());
+        const QJsonObject root = doc.object();
+        const QJsonObject current = root["current"].toObject();
+        const QJsonObject daily = root["daily"].toObject();
+        QJsonArray tempMax = daily["temperature_2m_max"].toArray();
+        QJsonArray humidityMean = daily["relative_humidity_2m_mean"].toArray();
+
+        QVariantList tempHistory;
+        QVariantList humidityHistory;
+
+        for (int i =0; i < tempMax.size(); i++){
+            tempHistory.append(tempMax[i].toDouble());
+            humidityHistory.append(humidityMean[i].toDouble());
+        }
+
+        m_environment->setTemperatureHistory(tempHistory);
+        m_environment->setHumidityHistory(humidityHistory);
 
         reply->deleteLater();
 
