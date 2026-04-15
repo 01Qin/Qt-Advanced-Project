bb9240bfe7901cf3bd7be4ed47ef623066c5f478
01Qin <sisukasqin@hotmail.com>
2026-04-15 20:46:08 +0300

    Change variable name

--- EnvironmentModel.cpp
+++ EnvironmentModel.cpp
@@ -4,23 +4,29 @@ EnvironmentModel::EnvironmentModel(QObject *parent)
     : QObject (parent) {}
 
 double EnvironmentModel::humidity () const {return m_humidity;}
-double EnvironmentModel::temperature () const {return m_temperature;}
+double EnvironmentModel::temp () const {return m_temp;}
 QString EnvironmentModel::source() const {return m_source;}
+
 
 void EnvironmentModel::setHumidity (double value){
     if (m_humidity == value) return;
     m_humidity = value;
-    emit dataChanged();
+    emit humidityChanged(m_humidity);
+    emit humidityHistoryChanged();
 }
 
-void EnvironmentModel::setTemperature(double value){
-    if(m_temperature == value) return;
-    m_temperature = value;
-    emit dataChanged();
+void EnvironmentModel::setTemp(double value){
+    if(m_temp == value) return;
+    m_temp = value;
+    emit tempChanged(m_temp);
+    emit tempHistoryChanged();
 }
 
 void EnvironmentModel::setSource (const QString &value){
     if (m_source == value) return;
     m_source = value;
-    emit dataChanged();
+    emit sourceChanged(m_source);
+
 }
+
+
