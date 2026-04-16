5aa0b7a06c6f2c3f51b7d79ccf8774ef479b2a64
01Qin <sisukasqin@hotmail.com>
2026-04-16 22:49:58 +0300

    Fix typo

--- OpenMeteo.cpp
+++ OpenMeteo.cpp
@@ -25,43 +25,11 @@ void OpenMeteo::stop(){
 }
 
 void OpenMeteo::fetchData(){
-    QUrl url("https://api.open-meteo.com/v1/forecast?latitude=60.2276"
-             "&longitude=24.8873&daily=temperature_2m_max,temperature_2m_min"
-             "&hourly=temperature_2m,relative_humidity_2m"
-             "&current=temperature_2m,relative_humidity_2m,is_day&timezone=auto"
-
-             );
-
-    QNetworkRequest request(url);
-    auto reply = m_network.get(request);
-
-    connect(reply, &QNetworkReply::finished, this, [this,reply] {
-        if (reply->error() != QNetworkReply::NoError){
-            reply->deleteLater();
-            return;
-        }
-
-        const QJsonDocument doc = QJsonDocument::fromJson(reply->readAll());
-        const QJsonObject root = doc.object();
-        const QJsonObject current = root["current"].toObject();
-
-        double temp = current["temperature_2m"].toDouble();
-        double humidity = current["relative_humidity_2m"].toDouble();
-        m_environment->setTemp(temp);
-        m_environment->setHumidity(humidity);
-        m_environment->setSource("Open-Meteo");
-
-        reply->deleteLater();
-
-    });
-}
-
-void OpenMeteo::fetchHistoryData(){
     QUrl url("https://api.open-meteo.com/v1/forecast"
-             "?latitude=60.2276&longitude=24.8873"
-             "&past_days=10"
-             "&daily=temperature_2m_max,temperature_2m_min"
-             "&relative_humidity_2m_mean"
+             "?latitude=60.2276"
+             "&longitude=24.8873"
+             "&current_weather=true"
+             "&hourly=temperature_2m,relative_humidity_2m"
              "&timezone=auto"
 
              );
@@ -77,8 +45,45 @@ void OpenMeteo::fetchHistoryData(){
 
         const QJsonDocument doc = QJsonDocument::fromJson(reply->readAll());
         const QJsonObject root = doc.object();
-        const QJsonObject current = root["current"].toObject();
-        const QJsonObject daily = root["daily"].toObject();
+
+        const QJsonObject currentWeather = root["current_weather"].toObject();
+        double temp = currentWeather["temperature_2m"].toDouble();
+
+        const QJsonObject hourly = root["hourly"].toObject();
+        const QJsonArray humidityArr = hourly["relative_humidity_2m"].toArray();
+        double humidity = humidityArr.isEmpty() ? 0.0 : humidityArr.first().toDouble();
+
+        m_environment->setTemp(temp);
+        m_environment->setHumidity(humidity);
+        m_environment->setSource("Open-Meteo");
+
+        reply->deleteLater();
+
+    });
+}
+
+void OpenMeteo::fetchHistoryData(){
+    QUrl url("https://api.open-meteo.com/v1/forecast"
+             "?latitude=60.2276&longitude=24.8873"
+             "&past_days=10"
+             "&daily=temperature_2m_max"
+             "&relative_humidity_2m_mean"
+             "&timezone=auto"
+
+             );
+
+    QNetworkRequest request(url);
+    auto reply = m_network.get(request);
+
+    connect(reply, &QNetworkReply::finished, this, [this,reply] {
+        if (reply->error() != QNetworkReply::NoError){
+            reply->deleteLater();
+            return;
+        }
+
+        const QJsonDocument doc = QJsonDocument::fromJson(reply->readAll());
+
+        const QJsonObject daily = doc.object()["daily"].toObject();
         QJsonArray tempMax = daily["temperature_2m_max"].toArray();
         QJsonArray humidityMean = daily["relative_humidity_2m_mean"].toArray();
 
@@ -86,9 +91,9 @@ void OpenMeteo::fetchHistoryData(){
             m_environment->setTemp(tempMax[i].toDouble());
             m_environment->setHumidity(humidityMean[i].toDouble());
 
-
-        reply->deleteLater();
         }
+        reply->deleteLater();
+
 
     });
 }
