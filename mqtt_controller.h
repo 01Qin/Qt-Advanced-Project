42d1b55a34ccbcd504922d6a6f8b2192edacafcc
01Qin <sisukasqin@hotmail.com>
2026-04-23 19:30:28 +0300

    Update Mqtt class

--- mqtt_controller.h
+++ mqtt_controller.h
@@ -3,25 +3,31 @@
 
 #pragma once
 #include <QObject>
+#include <QMqttClient>
 
 class MqttController : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(bool mistOn READ mistOn  NOTIFY mistOnChanged )
+    Q_PROPERTY(bool connected READ connected  NOTIFY connectedChanged FINAL)
 
 public:
-    explicit MqttController (QObject *parent = nullptr);
+    explicit MqttController (QMqttClient *client, QObject *parent = nullptr);
     bool mistOn() const { return m_mistOn; }
+    bool connected() const { return m_connected; }
 
 signals:
     void mistOnChanged();
+    void connectedChanged();
 
 public slots:
     void handleMqttmsg(const QString &topic, const QByteArray &payload);
     Q_INVOKABLE void setMist (bool on);
 
 private:
+    QMqttClient *mqttClient = nullptr;
     bool m_mistOn = false;
+    bool m_connected = false;
 };
 
 #endif // MQTT_CONTROLLER_H
