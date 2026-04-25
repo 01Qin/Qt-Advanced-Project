87a18e0634b05092f67ba1c718c4b9149268067e
01Qin <sisukasqin@hotmail.com>
2026-04-25 03:30:51 +0300

    Fix typo

--- mqtt_controller.cpp
+++ mqtt_controller.cpp
@@ -3,21 +3,22 @@
 MqttController::MqttController (QMqttClient *client, QObject *parent)
     : QObject(parent), m_client(client)
 {
-    connect (m_client, &QMqttClient :: stateChanged, this, QMqttClient :: ClientState state{
+    connect (m_client, &QMqttClient :: stateChanged, this, [this] (QMqttClient :: ClientState state) {
         bool nowConnected = (state == QMqttClient :: Connected);
         if (m_connected != nowConnected) {
             m_connected = nowConnected;
             emit connectedChanged();
+        }
                                                          });
 
     // receive message
-    connect(m_client, &QMqttClient::msgReceived, this, &MqttController::handleMqttmsg);
+    connect(m_client, &QMqttClient::messageReceived, this, &MqttController::handleMqttmsg);
 }
 
 
-    void MqttController ::handleMqttmsg(const QString &topic, const QByteArray &payload)
+    void MqttController ::handleMqttmsg(const QByteArray &payload, const QMqttTopicName &topic)
     {
-        if (topic == "terrarium/mist"){
+        if (topic.name() == "terrarium/mist"){
             bool newState = (payload == "ON");
 
             if (m_mistOn != newState){
@@ -29,7 +30,7 @@ MqttController::MqttController (QMqttClient *client, QObject *parent)
 
     void MqttController::setMist(bool on){
         if (m_client) {
-            m_client->publish("terrarium/mist", on ? "ON" : "OFF");
+            m_client->publish(QMqttTopicName("terrarium/mist"), on ? QByteArray("ON") : QByteArray("OFF"));
         }
 
         if (m_mistOn != on){
